73e4491303e71eb4de6aafb86c1a2c33db4245d2
craigthelinguist <sinroth@gmail.com>
2015-03-08 21:03:03 +1300

    improved, fixed bugs, added function signatures

--- primes.hs
+++ primes.hs
@@ -1,14 +1,47 @@
-primeFactors n = 1:[x | x <- 2:[3,5..upper], mod n x == 0] ++ [n]
-				 where upper = ceiling (sqrt (fromIntegral n))
 
-isPrime n = length (primeFactors n) == 2
 
-firstNPrimes n = findPrimes 2 n []
+-- Return the prime factors of a number.
+primeFactors :: Integer -> [Integer]
+primeFactors n
+  | n < 2     = []
+  | otherwise = [x | x <- (specialCases n) ++ [3,5..upper], isPrime x, x `divides` n]
+    where
+      specialCases n  = if isPrime n && n /= 2
+                          then [2,n]
+                        else if isPrime n
+                          then [n]
+                        else [2]
+      upper           = ceiling ( (toRational n) / 2)
+prime
+-- Return true if a divides b (that is k*a = b for some k).
+divides :: Integer -> Integer -> Bool
+divides a b = b `mod` a == 0
 
-findPrimes number stop xs
-	| length xs == stop		= xs
-	| number ==	2			= findPrimes 3 stop [2]
-	| otherwise				=
-		if isPrime number
-			then findPrimes (number + 2) stop (number:xs)
-			else findPrimes (number + 2) stop xs
+-- Return True if the input is prime, or false otherwise.
+isPrime :: Integer -> Bool
+isPrime n
+  | n == 2    = True
+  | n == 1    = False
+  | otherwise = isPrime' n 2
+  where isPrime' :: Integer -> Integer -> Bool
+        isPrime' n curr
+          | curr == 2 = if 2 `divides` n
+                          then False
+                          else isPrime' n 3
+          | otherwise = if curr > (ceiling (sqrt (fromIntegral n)))
+                          then True
+                        else if curr `divides` n
+                          then False
+                        else
+                          isPrime' n (curr+2)
+
+-- Find the first n primes.
+sieve :: Integer -> [Integer]
+sieve n = reverse (sieve' 2 n [])
+  where sieve' :: Integer -> Integer -> [Integer] -> [Integer]
+        sieve' curr stop primes
+          | toInteger (length primes) == n  = primes
+          | curr == 2             = sieve' 3 stop (2:primes)
+          | otherwise             = if isPrime curr
+                                    then sieve' (curr+2) stop (curr:primes)
+                                    else sieve' (curr+2) stop primes
